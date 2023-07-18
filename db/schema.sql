6d087b0fc24223aaf7ad79052595d2c7eff497b2
adamjfox <adamjeffersonfox@gmail.com>
2023-07-18 15:34:25 -0700

    schema done and server added

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+-- Drop the database if it already exists
+DROP DATABASE IF EXISTS employeesData;
+
+-- Create a new database named employeesData
+CREATE DATABASE employeesData;
+
+-- Use the employeesData database
+USE employeesData;
+
+-- Create the department table
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    dept_name VARCHAR(50) NOT NULL
+);
+
+-- Create the roles table
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50),
+    department_id INT,
+    salary DECIMAL(10, 2),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+-- Create the employee table
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
